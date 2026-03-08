b3915951fbd0e3e7267c4e0b00124a107769d44d
optshj <optshj@gmail.com>
2026-03-08 16:01:11 +0900

    🔍️ [SEO] metadata수정

--- src/app/metadata/index.ts
+++ src/app/metadata/index.ts
@@ -1,6 +1,7 @@
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://appcenter-hompage-renewal-web.inuappcenter.workers.dev'),
   title: '인천대학교 앱센터 | INU App Center - Student Developers Community',
   description:
     '인천대학교 학생들이 직접 애플리케이션과 서비스를 만드는 IT 이노베이션 랩(구 앱센터)의 공식 홈페이지입니다. 오랫동안 사용되어 온 앱센터가 정보전산원 산하의 AI 빅데이터 센터에 소속된 IT 이노베이션 랩으로 명칭 정식 변경되었습니다. 앱센터는 인천대학교 학생들이 애플리케이션과 서비스를 직접 만드는 공간입니다. 활동에 필요한 비용의 일부를 소속 기관으로부터 지원받고 있습니다.',
