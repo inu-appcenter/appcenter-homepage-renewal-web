0464fd730e3b50c90e82921ab8bc6305761c3a02
optshj <optshj@gmail.com>
2026-03-28 13:58:33 +0900

    🩹 [Fix] lading.png -> lading.webp로 변경

--- src/app/layout/RootLayout.tsx
+++ src/app/layout/RootLayout.tsx
@@ -2,8 +2,6 @@ import { Toaster } from 'shared/ui/toast';
 import { ReactQueryProvider } from '../provider/ReactQueryProvider';
 import { productDesignFont, pretendardFont, tokyoFont } from '../style/fonts';
 import '../style/globals.css';
-import { Analytics } from '@vercel/analytics/next';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const jsonLd = [
   {
@@ -31,8 +29,6 @@ export function RootLayout({ children }: Readonly<{ children: React.ReactNode }>
   return (
     <html lang="ko" className={`${productDesignFont.variable} ${pretendardFont.variable} ${tokyoFont.variable} no-scrollbar`}>
       <body className="antialiased">
-        <Analytics />
-        <SpeedInsights />
         <Toaster />
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
         <ReactQueryProvider>{children}</ReactQueryProvider>
