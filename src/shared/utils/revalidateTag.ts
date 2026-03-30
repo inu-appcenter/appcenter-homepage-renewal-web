af24b26f79ba933b7a51a02e991dbfe903dd1c9b
optshj <optshj@gmail.com>
2026-03-30 17:33:17 +0900

    ⚡️ [Feat] 프로젝트, 활동, 워크숍 데이터 로드 시 Nextjs 캐싱적용

--- src/shared/utils/revalidateTag.ts
+++ src/shared/utils/revalidateTag.ts
@@ -2,6 +2,5 @@
 import { revalidateTag as NextRevalidateTag } from 'next/cache';
 
 export async function revalidateTag(tag: string) {
-  console.log(`[Cache] Revalidating tag: ${tag}`);
-  NextRevalidateTag('workshops', 'default');
+  NextRevalidateTag(tag, 'default');
 }
