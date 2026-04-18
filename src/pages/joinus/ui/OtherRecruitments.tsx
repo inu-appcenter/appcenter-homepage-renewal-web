73bf44ea428c867ec733ed5bdbf61c5f8cbd0157
optshj <optshj@gmail.com>
2026-04-18 13:46:04 +0900

    💄 [Style] 디자인 수정사항 반영

--- src/pages/joinus/ui/OtherRecruitments.tsx
+++ src/pages/joinus/ui/OtherRecruitments.tsx
@@ -1,11 +1,11 @@
 'use client';
-import { useCallback, useEffect, useState } from 'react';
-import { ChevronLeft, ChevronRight, Menu } from 'lucide-react';
+import { Menu } from 'lucide-react';
 import Link from 'next/link';
 import useEmblaCarousel from 'embla-carousel-react';
 import { useRecruitment } from 'entities/recruitment';
 import { RecruitmentCard } from './Component';
 import { AsyncBoundary } from 'shared/error/AsyncBoundary';
+import Autoplay from 'embla-carousel-autoplay';
 
 export function OtherRecruitments() {
   return (
@@ -18,35 +18,20 @@ export function OtherRecruitments() {
 function OtherRecruitmentsContent() {
   const { data } = useRecruitment();
 
-  const [emblaRef, emblaApi] = useEmblaCarousel({
-    align: 'center',
-    containScroll: 'trimSnaps',
-    dragFree: true
-  });
-
-  const [prevBtnDisabled, setPrevBtnDisabled] = useState(true);
-  const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
-
-  const scrollPrev = useCallback(() => emblaApi && emblaApi.scrollPrev(), [emblaApi]);
-  const scrollNext = useCallback(() => emblaApi && emblaApi.scrollNext(), [emblaApi]);
-
-  const onSelect = useCallback(() => {
-    if (!emblaApi) return;
-    setPrevBtnDisabled(!emblaApi.canScrollPrev());
-    setNextBtnDisabled(!emblaApi.canScrollNext());
-  }, [emblaApi]);
-
-  useEffect(() => {
-    if (!emblaApi) return;
-    emblaApi.on('select', onSelect);
-    emblaApi.on('reInit', onSelect);
-    emblaApi.on('init', onSelect);
-    return () => {
-      emblaApi.off('select', onSelect);
-      emblaApi.off('reInit', onSelect);
-      emblaApi.off('init', onSelect);
-    };
-  }, [emblaApi, onSelect]);
+  const [emblaRef] = useEmblaCarousel(
+    {
+      align: 'center',
+      dragFree: true,
+      containScroll: 'trimSnaps'
+    },
+    [
+      Autoplay({
+        delay: 3000,
+        stopOnInteraction: false,
+        stopOnMouseEnter: true
+      })
+    ]
+  );
 
   if (!data || data.length === 0) return null;
 
@@ -66,22 +51,6 @@ function OtherRecruitmentsContent() {
       </div>
 
       <div className="relative w-full">
-        <button
-          onClick={scrollPrev}
-          disabled={prevBtnDisabled}
-          className={`text-brand-primary-cta disabled:text-custom-gray-800 border-custom-gray-800 bg-custom-black absolute top-1/2 -left-3 z-10 -translate-x-1/2 -translate-y-1/2 rounded-full border p-2 shadow-[0px_0px_16px_0px_#00000040] backdrop-blur-md transition-all hover:bg-white/20 disabled:pointer-events-none sm:-left-10`}
-        >
-          <ChevronLeft className="h-4 w-4 sm:h-8 sm:w-8" />
-        </button>
-
-        <button
-          onClick={scrollNext}
-          disabled={nextBtnDisabled}
-          className={`text-brand-primary-cta disabled:text-custom-gray-800 border-custom-gray-800 bg-custom-black absolute top-1/2 -right-3 z-10 translate-x-1/2 -translate-y-1/2 rounded-full border p-2 shadow-[0px_0px_16px_0px_#00000040] backdrop-blur-md transition-all hover:bg-white/20 disabled:pointer-events-none sm:-right-10`}
-        >
-          <ChevronRight className="h-4 w-4 sm:h-8 sm:w-8" />
-        </button>
-
         <div className="overflow-hidden py-4" ref={emblaRef}>
           <div className="flex gap-3 sm:gap-8">
             {data.map((item) => (
