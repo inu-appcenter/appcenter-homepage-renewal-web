91b959479af46b13a98ad3cf346f46ffe753096b
optshj <optshj@gmail.com>
2026-05-09 16:17:56 +0900

    ✏️ [Fix] 프로젝트 상세페이지 반응형 폰트 추가

--- src/pages/project/ui/MarkdownSection.tsx
+++ src/pages/project/ui/MarkdownSection.tsx
@@ -5,7 +5,7 @@ import remarkGfm from 'remark-gfm';
 
 export const MarkdownSection = ({ data }: { data: Project }) => {
   return (
-    <div className="prose prose-slate text-custom-gray-100 prose-headings:text-custom-gray-100 prose-strong:text-custom-gray-100 prose-headings:font-bold prose-a:text-white prose-blockquote:text-white prose-blockquote:border-white max-w-none text-xl leading-8 font-semibold">
+    <div className="prose prose-slate text-custom-gray-100 prose-headings:text-custom-gray-100 prose-strong:text-custom-gray-100 prose-headings:font-bold overflow-wrap-anywhere prose-a:text-white prose-blockquote:text-white prose-blockquote:border-white w-full max-w-none text-base/6 font-semibold wrap-break-word sm:text-xl/8">
       <ReactMarkdown remarkPlugins={[remarkGfm]}>{data.body}</ReactMarkdown>
     </div>
   );
